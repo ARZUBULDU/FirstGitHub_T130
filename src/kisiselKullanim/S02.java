bb22de6b2b0ad3301f7e243a35efd3dbcf8805cc
ABuldu <arzubulduu@gmail.com>
2023-12-09 21:08:33 +0300

    2.gun

---
+++ src/kisiselKullanim/S02.java
@@ -0,0 +1,9 @@
+package kisiselKullanim;
+
+public class S02 {
+    public static void main(String[] args) {
+        int a=128;
+        int b=256;
+        System.out.println("İki sayının toplamı :"+ (a + b));
+    }
+}
